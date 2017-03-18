56da1381f65d275bc030783676b6a76122f8c52d
Blakjak <blakjak44@gmail.com>
2017-03-17 19:29:15 -0700

    updated eye and stitching code

--- python/analyze_eye.py
+++ python/analyze_eye.py
@@ -5,47 +5,32 @@ import tifffile as tif
 
 def analyze_eye(fname,write=0):
     '''
-    Calculates pupil area and saves as .npy file.
+    Calculates pupil area and angular rotation  and saves data as .npy file.
     If write = 1, will also write out data with tracked pupil.
     '''
+    
+    bounding_region = 80 # restricts pupil search to +/- this value from the center
+    r_effective = 1.25
+    pixels_per_mm = 100.0
+
     # read in eye data
     data = h5py.File(fname + '.mat')
     eye_data = np.squeeze(np.array(data['data'])).transpose(0,2,1)
-    eye_data_center = np.array(eye_data.shape[1:3])/2
-    area_trace = np.zeros(eye_data.shape[0])
-    centroid_trace = np.int32(np.zeros([eye_data.shape[0],2])) # centroid is in xy coordinates
-    r_effective = 1.25
-    pixels_per_mm = 100
+    
+    eye_data_center = np.array(eye_data.shape[1:3])/2 # find center of entire frame
+    area_trace = np.zeros(eye_data.shape[0]) # pupillary area in mm^2
+    centroid_trace = np.int32(np.zeros([eye_data.shape[0],2])) # position of centroid in xy coordinates
+    x_offset = (eye_data.shape[2] - 2*bounding_region)/2 # x distance between full frame edge and cropped edge
+    y_offset = (eye_data.shape[1] - 2*bounding_region)/2 # y distance between full frame edge and cropped edge
+    center = np.array([bounding_region,bounding_region]) # center of cropped data
 
     if write:
         rgb_eye_data = np.uint8(np.zeros([eye_data.shape[0],eye_data.shape[1],eye_data.shape[2],3]))
 	for i in range(eye_data.shape[0]):
-            # find center of eye
-           #ret,eye_thresh = cv2.threshold(eye_data[i].copy(),18,255,cv2.THRESH_BINARY)
-           #eye_contours,hierarchy = cv2.findContours(eye_thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-           ##eye_hulls = [cv2.convexHull(eye_contour) for eye_contour in eye_contours]
-           #eye_areas = [cv2.contourArea(eye_contour) for eye_contour in eye_contours]
-           #eye_contours = [eye_contour for eye_contour,eye_area in zip(eye_contours,eye_areas) if eye_area > 100]
-           #eye_hulls = [cv2.convexHull(eye_contour) for eye_contour in eye_contours]
-           #eye_areas = [cv2.contourArea(eye_hull) for eye_hull in eye_hulls]
-           #eye_hulls = [eye_hull for eye_hull,eye_area in zip(eye_hulls,eye_areas) if eye_area > 3000 and eye_area < 7000]
-           #if eye_hull == []:
-           ## 'Most likely a blink.'
-           #    eye_hull = np.array([[0,0]])
-           #    centroid = eye_data_center[::-1]
-           #else:
-           #    #eye_hull = eye_hull[eye_areas.index(min(eye_areas))]
-           #    M = [cv2.moments(eye_hull) for eye_hull in eye_hulls]
-           #    centroids = [[int(m['m10']/m['m00']),int(m['m01']/m['m00'])] if m['m00'] != 0 else [0,0] for m in M]
-           #    eye_dist_list = np.sum(np.abs(eye_data_center[::-1] - centroids),1)
-           #    centroid = centroids[np.where(eye_dist_list == min(eye_dist_list))[0][0]]
             # crop eye data
-	    eye_frame = eye_data[i,eye_data_center[0]-80:eye_data_center[0]+80,eye_data_center[1]-80:eye_data_center[1]+80].copy()
-	    # determine location of centroid in cropped data
-	    x_offset = (eye_data[0].shape[1] - eye_frame.shape[1])/2
-	    y_offset = (eye_data[0].shape[0] - eye_frame.shape[0])/2
-            center = (np.array(eye_frame.shape)/2)[::-1]
-	    #center = np.array([centroid[0]-x_offset,centroid[1]-y_offset])
+	    eye_frame = eye_data[i,
+	        eye_data_center[0]-bounding_region:eye_data_center[0]+bounding_region,
+                eye_data_center[1]-bounding_region:eye_data_center[1]+bounding_region].copy()
 	    # threshold the image
 	    ret,thresh = cv2.threshold(eye_frame.copy(),44,255,cv2.THRESH_BINARY)
 	    # find contours
@@ -81,34 +66,32 @@ def analyze_eye(fname,write=0):
 		(raw_pupil_centroid[0]+x_offset + 2, raw_pupil_centroid[1]+y_offset + 2), 
 		(0, 255, 0), 
 		-1)
-            # draw eye contour
-            #img = cv2.drawContours(rgb_eye_frame, [eye_hull], 0, (0,128,255), 1)
             # draw eye centroid
             #cv2.rectangle(rgb_eye_frame,(centroid[0] - 2, centroid[1] - 2), (centroid[0] + 2, centroid[1] + 2), (0, 128, 255), -1)            
 	    rgb_eye_data[i] = rgb_eye_frame
-        
-	angular_rotation = np.arcsin((centroid_trace/pixels_per_mm)/r_effective) # trace is in form (Eh, Ev)
-       
-	np.save(fname + '_area_trace',area_trace)
-	np.save(fname + 'raw_xy_position',centroid_trace)
-	np.save(fname + 'angular_rotation',angular_rotation)
+
+	angular_rotation = np.zeros(centroid_trace.shape) 
+	angular_rotation[:,0] = np.arcsin((centroid_trace[:,0]/pixels_per_mm)/r_effective) # Eh in radians
+	angular_rotation[:,1] = np.arcsin((centroid_trace[:,1]/pixels_per_mm)/r_effective) # Ev in radians
+        angular_rotation = np.rad2deg(angular_rotation) # (Eh,Ev) into degrees
+
+	np.save(fname + '_pupil_area',area_trace)
+	#np.save(fname + '_raw_xy_position',centroid_trace) # don't need to save this, saving angular velocity instead
+	np.save(fname + '_angular_rotation',angular_rotation)
 	tif.imsave(fname + '_tracked.tif',rgb_eye_data)
     else:
         for i in range(eye_data.shape[0]):
-	    #crop eye data
-	    eye_frame = eye_data[i,eye_data_center[0]-80:eye_data_center[0]+80,eye_data_center[1]-80:eye_data_center[1]+80].copy()
-	    # determine location of centroid in cropped data
-	    x_offset = (eye_data[0].shape[1] - eye_frame.shape[1])/2
-	    y_offset = (eye_data[1].shape[0] - eye_frame.shape[0])/2
-            center = (np.array(eye_frame.shape)/2)[::-1]
-	    #center = np.array([centroid[0]-x_offset,centroid[1]-y_offset])
+            # crop eye data
+	    eye_frame = eye_data[i,
+	        eye_data_center[0]-bounding_region:eye_data_center[0]+bounding_region,
+                eye_data_center[1]-bounding_region:eye_data_center[1]+bounding_region].copy()
 	    # threshold the image
 	    ret,thresh = cv2.threshold(eye_frame.copy(),44,255,cv2.THRESH_BINARY)
 	    # find contours
-	    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	    # find convex hulls
 	    hulls = [cv2.convexHull(contour) for contour in contours]
-	    # find areas of hulls and filter by size
+	    # find areas of hulls
 	    areas = [cv2.contourArea(hull) for hull in hulls]
 	    # filter hulls by area size
 	    hulls = [hull for hull,area in zip(hulls,areas) if area > 100 and area < 25000]
@@ -117,10 +100,20 @@ def analyze_eye(fname,write=0):
             # find moments of remaining hulls
 	    M = [cv2.moments(hull) for hull in hulls]
 	    # find centers of each moment
-	    centers = [[int(m['m10']/m['m00']),int(m['m01']/m['m00'])] if m['m00'] != 0 else [0,0] for m in M]
-	    # select hull with center that is closest to image center and calculate area
+	    centers = [[int(m['m10']/m['m00']),int(m['m01']/m['m00'])] if m['m00'] != 0 else [0,0] for m in M] 
+            # select hull with center that is closest to image center and calculate area
 	    dist_list = np.sum(np.abs(centers - center),1)
 	    area_trace[i] = areas[np.where(dist_list == min(dist_list))[0][0]]
-            centroid_trace[i] = centers[np.where(dist_list == min(dist_list))[0][0]] - center
-        np.save(fname + '_area_trace',area_trace)
-        np.save(fname + '_centroid_position',centroid_trace)
+            raw_pupil_centroid = centers[np.where(dist_list == min(dist_list))[0][0]]
+            # store pupil centroid
+            centroid_trace[i] = raw_pupil_centroid - center
+            centroid_trace[i,1] = -centroid_trace[i,1]
+
+	angular_rotation = np.zeros(centroid_trace.shape) 
+	angular_rotation[:,0] = np.arcsin((centroid_trace[:,0]/pixels_per_mm)/r_effective) # Eh in radians
+	angular_rotation[:,1] = np.arcsin((centroid_trace[:,1]/pixels_per_mm)/r_effective) # Ev in radians
+        angular_rotation = np.rad2deg(angular_rotation) # (Eh,Ev) into degrees
+
+	np.save(fname + '_pupil_area',area_trace)
+	#np.save(fname + '_raw_xy_position',centroid_trace) # don't need to save this, saving angular velocity instead
+	np.save(fname + '_angular_rotation',angular_rotation)
--- python/recording_stitch.py
+++ python/recording_stitch.py
@@ -8,8 +8,8 @@ from pacu.core.io.scanbox.view.trial_merged_roi import TrialMergedROIViewByCentr
 class stitched_data(object):
     '''
     Create this object in directory that contains the .io files of interest.
-    fw_array should be list of tuples with the filename first and the workspace name second.
-    Example: ('Day1_000_000','Workspace_1'),('Day1_000_001',Workspace_1')
+    Argument should be list of tuples with the filename first and the workspace name second.
+    Example: stitched_dataset = stitched_data(['Day1_000_000','Workspace_1'),('Day1_000_001',Workspace_1')])
     '''
     def __init__(self,fw_array):
 	self.path = os.getcwd()
@@ -18,32 +18,11 @@ class stitched_data(object):
         self.rois = [workspace.rois for data, fw in zip(self.io, fw_array) for workspace in data.condition.workspaces if workspace.name == fw[1]]
         self.merged_rois = [TrialMergedROIViewByCentroid(roi.centroid,*self.workspaces) for roi in self.rois[0]] 
         self.refresh_all()
-	self.roi_dict = self.make_roi_dict()
+	self.roi_dict = {'{}{}'.format('id_',merged_roi.rois[0].id):merged_roi.serialize() for merged_roi in self.merged_rois}
 	 
     def refresh_all(self):
         for merged_roi in self.merged_rois:
 	    merged_roi.refresh()
-
-    def make_roi_dict(self):
-        merged_rois_dict = {'{}{}'.format('id_',merged_roi.rois[0].id):merged_roi.serialize() for merged_roi in self.merged_rois}
-        #for merged_roi in self.merged_rois:
-	    #merged_rois_dict[str(merged_roi.rois[0].id)] = merged_roi.serialize() # for now, using the id of the first ROI as the merged id
-            #merged_roi_ref = merged_rois_dict[idx]
-            #merged_roi_ref
-	    #merged_roi_ref['centroid'] = merged_roi.centroid
-	    #merged_roi_ref['df/f'] = [dict(dff0.attributes.items()[5:]) for dff0 in merged_roi.dff0s] 
-            ##merged_roi_ref['df/f'] = [dff0.toDict() for dff0 in merged_roi.dff0s]
-	    ##merged_roi['anova_each'] = [anovaeach.attributes.items()[5:-1] for anovaeach in merged_roi.anovaeachs]
-	    ##merged_roi_ref['anova_each'] = [dtanovaeach.toDict() for dtanovaeach in merged_roi.dtanovaeachs]
-	    #merged_roi_ref['anova_each'] = [[dtanovaeach.attributes.items()[i] for i in [8,10,16,17]] for dtanovaeach in merged_roi.dtanovaeachs]  
-            #merged_roi_ref['orientation_best_prefs'] = dict([merged_roi.dtorientationbestprefs[0].attributes.items()[i] for i  in [10,16]])
-            ##merged_roi_ref['orientation_best_prefs'] = [dtorientationbestpref.toDict() for dtorientationbestpref in merged_roi.dtorientationbestprefs]
-	    ##merged_roi_ref['orientations_fits'] = [dtorientationsfit.toDict() for dtorientationsfit in merged_roi.dtorientationsfits]
-	    #merged_roi_ref['orientations_fits'] = [dict([dtorientationsfit.attributes.items()[i] for i in [8,10,16]]) for dtorientationsfit in merged_roi.dtorientationsfits]
-	    ##merged_roi_ref['orientations_means'] = [dtorientationsmean.toDict() for dtorientationsmean in merged_roi.dtorientationsmeans]
-	    #merged_roi_ref['orientations_means'] = [dict([dtorientationsmean.attributes.items()[i] for i in [8,10,16,17,18,19,20]]) for dtorientationsmean in merged_roi.dtorientationsmeans]
-	    ##merged_roi_ref['s.freq_fits'] = [dtsfreqfit.toDict() for dtsfreqfit in merged_roi.dtsfreqfits]
-        return merged_rois_dict
 
     def export_mat(self):
         merged_dict = {}
