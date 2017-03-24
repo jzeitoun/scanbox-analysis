9746fe247c7d14706f19cd95950208f4d6cbfb8d
Blakjak <blakjak44@gmail.com>
2017-03-23 17:24:55 -0700

    added option to align off second file

--- python/moco_mp_align.py
+++ python/moco_mp_align.py
@@ -13,13 +13,23 @@ from sbxread import *
 
 if __name__ == '__main__':
     
-    fname = sys.argv[1]
-    info = sbxread(fname)
-    mapped_data = np.memmap(fname + '.sbx', dtype='uint16', shape=(info['length'], info['sz'][0], info['sz'][1]))
+    filename = sys.argv[1]
+    if 'sbx' in filename:
+        filename = filename[:-4]
+
+    info = sbxread(filename)
+    mapped_data = sbxmap(filename + '.sbx') #np.memmap(filename + '.sbx', dtype='uint16', shape=(info['length'], info['sz'][0], info['sz'][1]))
     transform_file = tempfile.mktemp()
     transforms = np.memmap(transform_file, dtype='int64', mode = 'w+', shape =(info['length'],2))
-    template = np.uint16(np.mean(mapped_data[20:40,:], 0))
-    mapped_width = info['sz'][1]
+   
+    # if second filename is supplied, use template from that file for alignment
+    if len(sys.argv) > 2:
+        align_filename = sys.argv[2]
+        mapped_align_filename = sbxmap(align_filename + '.sbx')
+        template = np.uint16(np.mean(mapped_align_filename[20:40,:], 0))
+    else:
+        template = np.uint16(np.mean(mapped_data[20:40,:], 0))
+
     # need to crop left margin if data is bidirectional
     if info['scanmode'] == 0:
         info['sz'][1] = info['sz'][1]-100
@@ -29,7 +39,7 @@ if __name__ == '__main__':
    
 
     print 'Allocating space for aligned data...'
-    output_data = np.memmap('Moco_Aligned_' + fname + '.sbx', dtype='uint16', shape=(info['length'], info['sz'][0], info['sz'][1]), mode = 'w+')
+    output_data = np.memmap('Moco_Aligned_' + filename + '.sbx', dtype='uint16', shape=(info['length'], info['sz'][0], info['sz'][1]), mode = 'w+')
     
     num_cores = multiprocessing.cpu_count()/2
     #core_assignments = [[np.arange(core,mapped_data.shape[0],num_cores), mapped_data[core::num_cores]] for core in range(num_cores)]
@@ -38,7 +48,7 @@ if __name__ == '__main__':
     q = Queue()
     
     print 'Creating processes...'
-    processes = [Process(target=align_purepy, args=(fname,indices,template,ds_template, info['length'], info['sz'][0], mapped_width, info['sz'][1], transform_file, q, info['scanmode'])) for indices in core_assignments]
+    processes = [Process(target=align_purepy, args=(filename,indices,template,ds_template, info['length'], info['sz'][0], info['sz'][1], transform_file, q, info['scanmode'])) for indices in core_assignments]
     
     start = time.time()
 
@@ -59,15 +69,10 @@ if __name__ == '__main__':
             break
 
     max_aligned_idx = max(q_list)
-    elapsed_time = time.time() - start 
-
-    print 'Finished. Aligned %d frames in %d seconds' % (max_aligned_idx, elapsed_time)
-    print 'Alignment speed: %d frames/sec' % (max_aligned_idx/elapsed_time)
-
-    np.save('Moco_Aligned_' + fname + '_trans',transforms)
-    spio_info = loadmat(fname + '.mat')
-    spio_info['info']['sz'] = info['sz']
-    spio.savemat('Moco_Aligned_' + fname + '.mat',{'info':spio_info['info']})
+        
+    print 'Finished. Aligned %d frames in %d seconds' % (max_aligned_idx, time.time() - start)
+    
+    np.save('Moco_Aligned_' + filename + '_trans',transforms)
 
     del transforms
     _ = gc.collect()
