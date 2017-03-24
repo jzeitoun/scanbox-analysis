40be1b0a3f4b24cd7d2e29180aee545f15bf6801
Blakjak <blakjak44@gmail.com>
2017-03-24 02:05:26 -0700

    fixing "filename"

--- python/moco_mp_align.py
+++ python/moco_mp_align.py
@@ -1,4 +1,5 @@
-import multiprocessing
+i
+port multiprocessing
 from multiprocessing import Process, Queue
 from Queue import Empty
 import os
@@ -75,10 +76,10 @@ if __name__ == '__main__':
     print 'Finished. Aligned %d frames in %d seconds' % (max_aligned_idx, elapsed_time)
     print 'Alignment speed: %d frames/sec' % (max_aligned_idx/elapsed_time)
 
-    np.save('Moco_Aligned_' + fname + '_trans',transforms)
-    spio_info = loadmat(fname + '.mat')
+    np.save('Moco_Aligned_' + filename + '_trans',transforms)
+    spio_info = loadmat(filename + '.mat')
     spio_info['info']['sz'] = info['sz']
-    spio.savemat('Moco_Aligned_' + fname + '.mat',{'info':spio_info['info']})
+    spio.savemat('Moco_Aligned_' + filename + '.mat',{'info':spio_info['info']})
 
     del transforms
     _ = gc.collect()
