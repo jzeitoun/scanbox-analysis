e4acd911c5dcb2328ac8a2eb10b0a9494e6bad7e
Blakjak <blakjak44@gmail.com>
2017-03-29 17:47:33 -0700

    option to select export filename

--- python/recording_stitch.py
+++ python/recording_stitch.py
@@ -45,14 +45,18 @@ class stitched_data(object):
             sorted_orientation_traces[k] = [dict(dtorientationsmean.attributes.items()[i] for i in [8,17]) for roi in merged_roi.rois for dtorientationsmean in roi.dtorientationsmeans if roi.workspace.name == k]
 	return sorted_orientation_traces
     	
-    def export_mat(self):
+    def export_mat(self,filename=None):
         merged_dict = {}
 	merged_dict['filenames'] = [fw[0][:-3] for fw in self.fw_array]
 	merged_dict['workspaces'] = [workspace.name for workspace in self.workspaces]
 	merged_dict['rois'] = self.roi_dict 
 	for merged_roi in self.merged_rois: 
 	   merged_dict['rois']['{}{}'.format('id_',merged_roi.rois[0].id)]['sorted_dtorientationsmeans'] = self.sorted_orientation_traces(merged_roi)
-        sio.savemat(self.fw_array[0][0][:-3] + '_merged.mat',{'merged_dict':merged_dict})             
+        if filename == None:
+            fname = self.fw_array[0][0][:-3] + '_merged.mat'
+        else:
+            fname = filename
+        sio.savemat(fname,{'merged_dict':merged_dict})             
 
     def create_SFTP(self):
         hostname = '128.200.21.98' # glass.bio.uci.edu
