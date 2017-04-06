3cca1d0943ee852f361da989ee972dfdb4458adb
Blakjak <blakjak44@gmail.com>
2017-04-06 15:56:05 -0700

    update

--- python/velocity_relationship.py
+++ python/velocity_relationship.py
@@ -15,16 +15,37 @@ def find_relationship(io_file,_workspace,smoothwalk_file,eye1_data,eye2_data):
     conditions = [dict(t.attributes) for t in io.condition.trials]
     rois = [roi for roi in workspace.rois]
     framerate = io.condition.framerate
+    number_frames = rois[0].dtorientationsmeans.first.on_frames
+
+    on_times = np.array([d['on_time'] for d in conditions])                                
+    off_times = np.array([d['off_time'] for d in conditions])                              
+                                                                                          
+    on_frames = np.int64(on_times * framerate)                                              
+    off_frames = np.int64(off_times * framerate)                                            
+                                                                                          
+    on_idx = zip(on_frames,on_frames+number_frames+1)                                       
+    #off_idx = zip(off_frames,np.append(on_frames[1:],on_frames[-1] + min(np.diff(on_frames))))
 
     for roi in rois:
-        # creates tuples of on_frame and r_value 
+        # creates tuples of on_frame and r-value 
         r_value = [
                 (np.int64(trial.trial_on_time*framerate), 
                     np.mean(trial.value['on'])
                     )
                 for trial in roi.dttrialdff0s
                 ]
+        # create dataframe from r-values
         dataset = pd.DataFrame(r_value,columns=['on_frame','r-value'])
-
+        
+        # generate velocity data
         smooth_data = findSmoothVelocity(smoothwalk_file)
+        # adjust to zero-indexing
+        smooth_data['Count'] = np.int64(smooth_data['Count'])-1
+        # extract mean of velocity for on_frames
+        sorted_mean_velocity = [
+                (on[0],
+                    np.mean(smooth_data.set_index('Count').loc[on[0]:on[1]-1]['Velocity'].as_matrix())
+                    ) for on in on_idx
+                ]
+        # create dataframe from sorted_mean_velocities
 
