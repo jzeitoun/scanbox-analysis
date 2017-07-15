7b30922a89c4aba9fc7caab5afd799b59040bf42
jzeitoun <jzeitoun@uci.edu>
2017-07-14 19:07:46 -0700

    adding directional circular variance

--- python/recording_stitch.py
+++ python/recording_stitch.py
@@ -23,16 +23,16 @@ class stitched_data(object):
         self.io = [ScanboxIO(os.path.join(self.path,fw[0])) for fw in self.fw_array]
         self.condition = self.io[0].condition
         self.workspaces = [workspace for data,fw in zip(self.io, self.fw_array) for workspace in data.condition.workspaces if workspace.name == fw[1]]
-        self.rois = self.find_matched_rois() 
-        #self.merged_rois = [TrialMergedROIView(roi.id,*self.workspaces) for roi in self.rois[0]] 
-        self.merged_rois = [TrialMergedROIView(roi.params.cell_id,*self.workspaces) for roi in self.rois[0]] 
+        self.rois = self.find_matched_rois()
+        #self.merged_rois = [TrialMergedROIView(roi.id,*self.workspaces) for roi in self.rois[0]]
+        self.merged_rois = [TrialMergedROIView(roi.params.cell_id,*self.workspaces) for roi in self.rois[0]]
         self.refresh_all()
 	#self.roi_dict = {'{}{}'.format('id_',merged_roi.rois[0].id):merged_roi.serialize() for merged_roi in self.merged_rois}
         self.roi_dict = {'{}{}'.format('cell_id_',merged_roi.rois[0].params.cell_id):merged_roi.serialize() for merged_roi in self.merged_rois}
 	#self.sftp = self.create_SFTP()
     
     def find_matched_rois(self):
-        rois = [workspace.rois for data, fw in zip(self.io, self.fw_array) for workspace in data.condition.workspaces if workspace.name == fw[1]]    
+        rois = [workspace.rois for data, fw in zip(self.io, self.fw_array) for workspace in data.condition.workspaces if workspace.name == fw[1]]
         #id_sets = [[roi.id for roi in roi_list] for roi_list in rois]
         id_sets = [[roi.params.cell_id for roi in roi_list] for roi_list in rois]
         list_lengths = [len(s) for s in id_sets]
@@ -57,8 +57,8 @@ class stitched_data(object):
         merged_dict = {}
 	merged_dict['filenames'] = [fw[0][:-3] for fw in self.fw_array]
 	merged_dict['workspaces'] = [workspace.name for workspace in self.workspaces]
-	merged_dict['rois'] = self.roi_dict 
-	for merged_roi in self.merged_rois: 
+	merged_dict['rois'] = self.roi_dict
+	for merged_roi in self.merged_rois:
 	   #merged_dict['rois']['{}{}'.format('id_',merged_roi.rois[0].id)]['sorted_dtorientationsmeans'] = self.sorted_orientation_traces(merged_roi)
             merged_dict['rois']['{}{}'.format('cell_id_',merged_roi.rois[0].params.cell_id)]['sorted_dtorientationsmeans'] = self.sorted_orientation_traces(merged_roi)
         if filename == None:
@@ -66,7 +66,7 @@ class stitched_data(object):
         else:
             fname = filename + '.mat'
         # save .mat file
-        sio.savemat(fname,{'merged_dict':merged_dict})             
+        sio.savemat(fname,{'merged_dict':merged_dict})
        
         # create excel file
         wb = Workbook()
@@ -83,7 +83,8 @@ class stitched_data(object):
         ws.merge_cells('B1:C1')
         ws.merge_cells('D1:E1')
         ws.merge_cells('F1:I1')
-        ws.merge_cells('R1:S1')
+        #ws.merge_cells('R1:S1')
+        ws.merge_cells('S1:T1')
 
         for top,bottom in zip(ws['J1:Q1'][0],ws['J2:Q2'][0]):
             ws.merge_cells('{}{}:{}{}'.format(top.column,top.row,bottom.column,bottom.row))
@@ -156,33 +157,41 @@ class stitched_data(object):
         ws['D1'].style = header
         ws['F1'].value = 'SF'
         ws['F1'].style = header
-        ws['R1'].value = 'Anova Each'
-        ws['R1'].style = header
-        ws['R2'].value = 'F'
-        ws['R2'].style = header
-        ws['S2'].value = 'P'
+        #ws['R1'].value = 'Anova Each'
+        #ws['R1'].style = header
+        #ws['R2'].value = 'F'
+        #ws['R2'].style = header
+        #ws['S2'].value = 'P'
+        #ws['S2'].style = header
+        ws['S1'].value = 'Anova Each'
+        ws['S1'].style = header
+        ws['S2'].value = 'F'
         ws['S2'].style = header
+        ws['T2'].value = 'P'
+        ws['T2'].style = header
+
 
         for cell,val in zip(ws[2][1:9],['F','P','X','Y','Peak','Pref','Bandwidth','Global\nOPref']):
             cell.value = val
             cell.style = header
             
-        for cell,val in zip(ws[1][9:17],['@', 'OSI', 'CV', 'DSI', 'Sigma', 'OPref', 'RMax', 'Residual']):
+        for cell,val in zip(ws[1][9:18],['@', 'OSI', 'CV', 'DCV', 'DSI', 'Sigma', 'OPref', 'RMax', 'Residual']):
+        #for cell,val in zip(ws[1][9:17],['@', 'OSI', 'CV', 'DSI', 'Sigma', 'OPref', 'RMax', 'Residual']):
             cell.value = val
             cell.style = header
 
         for idx,roi in zip(idx_list,self.merged_rois):
             #if any([anovaeach.p < p_value for anovaeach in roi.dtanovaeachs]):
-            peak_sf = round(roi.dtsfreqfits[0].attributes['value']['peak'],2) 
+            peak_sf = round(roi.dtsfreqfits[0].attributes['value']['peak'],2)
             try:
                 if roi.dtanovaeachs.filter_by(trial_sf=peak_sf)[0].p <= p_value:
                     style = sig_cell
                 else:
                     style = reg_cell
             except IndexError:
-                style = reg_cell 
+                style = reg_cell
 
-            if unmerge == 0:    
+            if unmerge == 0:
                 for top,bottom in zip(ws['A{}:I{}'.format(idx,idx)][0],ws['A{}:I{}'.format(idx+num_sf-1,idx+num_sf-1)][0]):
                     ws.merge_cells('{}{}:{}{}'.format(top.column,top.row,bottom.column,bottom.row))
                 
@@ -199,16 +208,16 @@ class stitched_data(object):
                 except AttributeError:
                     print 'No "SF Cutoff Rel33" found for cell ',roi.rois[0].params.cell_id
                     ws.cell(row=idx,column=4).value = None
-                    ws.cell(row=idx,column=5).value = None 
-                ws.cell(row=idx,column=4).style = style   
+                    ws.cell(row=idx,column=5).value = None
+                ws.cell(row=idx,column=4).style = style
                 ws.cell(row=idx,column=5).style = style
-                ws.cell(row=idx,column=6).value = peak_sf 
+                ws.cell(row=idx,column=6).value = peak_sf
                 ws.cell(row=idx,column=6).style = style
                 ws.cell(row=idx,column=7).value = roi.dtsfreqfits.first.attributes['value']['pref']
                 ws.cell(row=idx,column=7).style = style
                 ws.cell(row=idx,column=8).value = roi.dtsfreqfits.first.attributes['value']['ratio']
                 ws.cell(row=idx,column=8).style = style
-                ws.cell(row=idx,column=9).value = roi.dtorientationbestprefs.first.attributes['value']  
+                ws.cell(row=idx,column=9).value = roi.dtorientationbestprefs.first.attributes['value']
                 ws.cell(row=idx,column=9).style = style
 
             elif unmerge == 1:
@@ -225,41 +234,71 @@ class stitched_data(object):
                     except AttributeError:
                         print 'No "SF Cutoff Rel33" found for cell ',roi.rois[0].params.cell_id
                         ws.cell(row=idx+i,column=4).value = None
-                        ws.cell(row=idx+i,column=5).value = None 
-                    ws.cell(row=idx+i,column=4).style = style   
+                        ws.cell(row=idx+i,column=5).value = None
+                    ws.cell(row=idx+i,column=4).style = style
                     ws.cell(row=idx+i,column=5).style = style
-                    ws.cell(row=idx+i,column=6).value = peak_sf 
+                    ws.cell(row=idx+i,column=6).value = peak_sf
                     ws.cell(row=idx+i,column=6).style = style
                     ws.cell(row=idx+i,column=7).value = roi.dtsfreqfits.first.attributes['value']['pref']
                     ws.cell(row=idx+i,column=7).style = style
                     ws.cell(row=idx+i,column=8).value = roi.dtsfreqfits.first.attributes['value']['ratio']
                     ws.cell(row=idx+i,column=8).style = style
-                    ws.cell(row=idx+i,column=9).value = roi.dtorientationbestprefs.first.attributes['value']  
+                    ws.cell(row=idx+i,column=9).value = roi.dtorientationbestprefs.first.attributes['value']
                     ws.cell(row=idx+i,column=9).style = style
            
             for i,cell in enumerate(ws.iter_rows(min_col=10, max_col=10, min_row=idx, max_row=idx+num_sf-1)):
                     cell[0].value = sfreqs[i]
                     cell[0].style = style
+            
+            xoris = [v for v in workspace.rois.first.dtorientationsmeans.first.attributes['indices'].values()]
+            xoris.sort()
+
+            indices = [k for k in workspace.rois.first.dtorientationsmeans.first.attributes['indices'].keys()]
+            indices.sort()
+            ymeas = np.array(workspace.rois.first.dtorientationsmeans.first.attributes['meantrace'])[indices]
+
+            sqrt, sin, cos, sum = np.sqrt, np.sin, np.cos, np.sum
+            thetas = (np.array(xoris)/360)*2*np.pi
+            R_thetas = np.array(ymeas)
+            dcv = sqrt(
+                    sum((R_thetas * sin(thetas)))**2 + sum((R_thetas * cos(thetas)))**2
+                                ) / sum(R_thetas)
 
             for i,row in enumerate(ws.iter_rows(min_col=11, max_col=19, min_row=idx, max_row=idx+num_sf-1)):
-                row[0].value = roi.dtorientationsfits[i].attributes['value']['osi']    
+                row[0].value = roi.dtorientationsfits[i].attributes['value']['osi']
                 row[0].style = style
                 row[1].value = roi.dtorientationsfits[i].attributes['value']['cv']
                 row[1].style = style
-                row[2].value = roi.dtorientationsfits[i].attributes['value']['dsi']
+                row[2].value = dcv
                 row[2].style = style
-                row[3].value = roi.dtorientationsfits[i].attributes['value']['sigma']
+                #row[2].value = roi.dtorientationsfits[i].attributes['value']['dsi']
+                #row[2].style = style
+                #row[3].value = roi.dtorientationsfits[i].attributes['value']['sigma']
+                #row[3].style = style
+                #row[4].value = roi.dtorientationsfits[i].attributes['value']['o_pref']
+                #row[4].style = style
+                #row[5].value = roi.dtorientationsfits[i].attributes['value']['r_max']
+                #row[5].style = style
+                #row[6].value = roi.dtorientationsfits[i].attributes['value']['residual']
+                #row[6].style = style
+                #row[7].value = roi.dtanovaeachs[i].attributes['f']
+                #row[7].style = style
+                #row[8].value = roi.dtanovaeachs[i].attributes['p']
+                #row[8].style = style
+                row[3].value = roi.dtorientationsfits[i].attributes['value']['dsi']
                 row[3].style = style
-                row[4].value = roi.dtorientationsfits[i].attributes['value']['o_pref']
+                row[4].value = roi.dtorientationsfits[i].attributes['value']['sigma']
                 row[4].style = style
-                row[5].value = roi.dtorientationsfits[i].attributes['value']['r_max']
+                row[5].value = roi.dtorientationsfits[i].attributes['value']['o_pref']
                 row[5].style = style
-                row[6].value = roi.dtorientationsfits[i].attributes['value']['residual']
+                row[6].value = roi.dtorientationsfits[i].attributes['value']['r_max']
                 row[6].style = style
-                row[7].value = roi.dtanovaeachs[i].attributes['f']
+                row[7].value = roi.dtorientationsfits[i].attributes['value']['residual']
                 row[7].style = style
-                row[8].value = roi.dtanovaeachs[i].attributes['p']
+                row[8].value = roi.dtanovaeachs[i].attributes['f']
                 row[8].style = style
+                row[9].value = roi.dtanovaeachs[i].attributes['p']
+                row[9].style = style
         
         # save excel file
         if filename == None:
@@ -271,7 +310,7 @@ class stitched_data(object):
     def create_SFTP(self):
         hostname = '128.200.21.98' # glass.bio.uci.edu
         username = 'ht'
-        password = 'mge2cortex' 
+        password = 'mge2cortex'
 
         ssh = SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
