96af32cbb2b681768b5e1ea88d1cd550e031de94
2P2 <2P2@glams.bio.uci.edu>
2018-02-08 13:23:56 -0800

    added batch tif convert

--- python/project.py
+++ python/project.py
@@ -18,22 +18,26 @@ def convert_sum(projection):
 def generate_projection(method):
     '''
     Walks through all subdirectories of the current folder and forms a projection
-    of all aligned sbx files using the specified method.
+    image of all aligned sbx files using the specified method.
     '''
     #tif_files = []
     for path, dirs, files in os.walk(os.getcwd()):
         sbx_files = [f for f in files if f.startswith('moco') and f.endswith('.sbx')]
         for sbx_file in sbx_files:
             full_sbx_path = os.path.join(path, sbx_file)
-            print('Processing: {}'.format(full_sbx_path))
             sbx_basename = os.path.splitext(full_sbx_path)[0]
-            sbx = sbxmap(full_sbx_path)
-            data = sbx.data()['green']['plane_0']
-            projection = projection_methods[method](~data, 0)
-            if method == 'sum':
-                projection = convert_sum(projection)
-            tif.save('{}_{}.tif'.format(sbx_basename, method), projection.astype('uint16'))
-            tif_files.append('{}_{}.tif'.format(sbx_basename, method))
+            tif_name = '{}_{}.tif'.format(sbx_basename, method)
+            if os.path.exists(tif_name) or '098_008_005' in tif_name:
+                print('{} exists, skipping'.format(tif_name))
+            else:
+                print('Processing: {}'.format(full_sbx_path))
+                sbx = sbxmap(full_sbx_path)
+                data = sbx.data()['green']['plane_0']
+                projection = projection_methods[method](~data, 0)
+                if method == 'sum':
+                    projection = convert_sum(projection)
+                tif.imsave('{}_{}.tif'.format(sbx_basename, method), projection.astype('uint16'))
+                #tif_files.append('{}_{}.tif'.format(sbx_basename, method))
 
     #with open('dryrun.txt', 'w') as f:
     #    for line in tif_files:
